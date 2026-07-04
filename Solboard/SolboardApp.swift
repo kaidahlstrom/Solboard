a154b3fe096591b61c8eabdbc55f90319a10f85b
Kai Dahlstorm <kaidahlstrom@gmail.com>
2026-07-04 16:57:25 -0600

    Initial Commit

---
+++ Solboard/SolboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolboardApp.swift
+//  Solboard
+//
+//  Created by kaidahlstrom on 2026-07-04.
+//
+
+import SwiftUI
+
+@main
+struct SolboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
